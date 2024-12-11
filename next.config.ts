01b722bfdf6cc87664896445baa70af3bb4d0c26
JiHoon-0330 <ljihoon0330@gamil.com>
2024-12-11 21:17:30 +0900

    chore: todo 저장할 파일 생성
    빌드나 개발서버 실행 시 todo 저장할 폴더, 파일이 존재하지 않는 경우 생성하기

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,28 @@
+import { TODO_FILE_PATH } from "@/src/Todo/api/constants";
+import { existsSync, mkdirSync, writeFileSync } from "fs";
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      const folderPath = path.join(__dirname, "data");
+
+      if (!existsSync(folderPath)) {
+        mkdirSync(folderPath, { recursive: true });
+        console.log("Folder created:", folderPath);
+      }
+
+      if (!existsSync(TODO_FILE_PATH)) {
+        writeFileSync(TODO_FILE_PATH, "[]", "utf-8");
+        console.log("Todo file created:", TODO_FILE_PATH);
+      }
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
